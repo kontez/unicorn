1b23dc4bd9c76dfa413dbd5a9965a3fddd6c7db1
Clément Cigix Gillard <clement.gillard@epita.fr>
2016-03-13 15:11:25 +0000

    Add song script

---
+++ song.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# displays the song and artist currently playing on twitch.tv/monstercat
+# relies on an homemade script accessible at cigix.noip.me/update.php
+
+from scrolldisp import *
+import unicornhat, urllib2, string
+
+unicornhat.rotation(180)
+unicornhat.brightness(0.05)
+
+disp = ScrollDisp("~R" + urllib2.urlopen("http://cigix.noip.me/update.php").read() + " ~R")
+disp.start()
